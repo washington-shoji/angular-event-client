a0f517d27dc540700fc7228ba9c86381f99346de
washington-shoji <shoji.washington@gmail.com>
2024-06-23 12:48:07 +1000

    - updated tail wind config, added http global client provider, updated router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { PublicLayoutScreenComponent } from './components/public-layout-screen/public-layout-screen.component';
 import { AdminLayoutScreenComponent } from './components/admin-layout-screen/admin-layout-screen.component';
+import { PublicEventPageComponent } from './pages/public-event-page/public-event-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: PublicLayoutScreenComponent,
-    children: [],
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'public-events' },
+      { path: 'public-events', component: PublicEventPageComponent },
+    ],
   },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
