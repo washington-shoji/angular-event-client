889bc3747bd9ab49e6c3c94ea84339cd6c89f502
washington-shoji <shoji.washington@gmail.com>
2024-06-23 20:30:08 +1000

    - added profile-dropdown component

---
+++ src/app/components/profile-dropdown/profile-dropdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-dropdown.component.html',
+  styleUrl: './profile-dropdown.component.scss'
+})
+export class ProfileDropdownComponent {
+
+}
