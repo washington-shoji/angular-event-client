99be899ac0afbf272c1f11dd39c5f61e2864bfba
washington-shoji <shoji.washington@gmail.com>
2024-06-23 20:27:26 +1000

    - implemented admin-layout component

--- src/app/components/admin-layout-screen/admin-layout-screen.component.ts
+++ src/app/components/admin-layout-screen/admin-layout-screen.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { AdminNavbarComponent } from '../admin-navbar/admin-navbar.component';
+import { AdminSidebarComponent } from '../admin-sidebar/admin-sidebar.component';
 
 @Component({
   selector: 'app-admin-layout-screen',
   standalone: true,
-  imports: [],
+  imports: [AdminNavbarComponent, AdminSidebarComponent],
   templateUrl: './admin-layout-screen.component.html',
-  styleUrl: './admin-layout-screen.component.scss'
+  styleUrl: './admin-layout-screen.component.scss',
 })
 export class AdminLayoutScreenComponent {
+  openSidebar: boolean = false;
 
+  toggleOpenSidebar($event: boolean): void {
+    this.openSidebar = !this.openSidebar;
+  }
 }
