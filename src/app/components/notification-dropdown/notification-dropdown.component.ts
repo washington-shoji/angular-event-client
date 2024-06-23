613a771531e8a23f1f476f6f1df92b262f6230c4
washington-shoji <shoji.washington@gmail.com>
2024-06-23 20:29:51 +1000

    - added notification-dropdown component

---
+++ src/app/components/notification-dropdown/notification-dropdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification-dropdown.component.html',
+  styleUrl: './notification-dropdown.component.scss'
+})
+export class NotificationDropdownComponent {
+
+}
