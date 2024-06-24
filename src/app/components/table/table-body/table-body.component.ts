7cd62e50179a42a7d6ae62c377a068be882bc4fb
washington-shoji <shoji.washington@gmail.com>
2024-06-24 20:39:52 +1000

    - added table-body component

---
+++ src/app/components/table/table-body/table-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-body.component.html',
+  styleUrl: './table-body.component.scss'
+})
+export class TableBodyComponent {
+
+}
