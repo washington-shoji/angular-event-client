b574d768019b02052f5072b5adc4bd1dbe72fe9a
washington-shoji <shoji.washington@gmail.com>
2024-06-24 20:40:15 +1000

    - added table-footer component

---
+++ src/app/components/table/table-footer/table-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-footer.component.html',
+  styleUrl: './table-footer.component.scss'
+})
+export class TableFooterComponent {
+
+}
