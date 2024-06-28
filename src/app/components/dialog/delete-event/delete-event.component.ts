45d643be1f340702b3ef8ad9ffad8cef9f1d4c43
washington-shoji <shoji.washington@gmail.com>
2024-06-28 18:28:10 +1000

    - updated delete-event component logic and template

--- src/app/components/dialog/delete-event/delete-event.component.ts
+++ src/app/components/dialog/delete-event/delete-event.component.ts
@@ -1,15 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { AppEvent } from '../../../pages/public-event-page/event.type';
+import { AdminEventService } from '../../../pages/admin/services/admin-event.service';
+import { take } from 'rxjs';
+import { ErrorAlertComponent } from '../../error-alert/error-alert.component';
+
+type RouteState = {
+  event: AppEvent;
+  navigationId: number;
+};
 
 @Component({
   selector: 'app-delete-event',
   standalone: true,
-  imports: [],
+  imports: [ErrorAlertComponent],
+  providers: [AdminEventService],
   templateUrl: './delete-event.component.html',
   styleUrl: './delete-event.component.scss',
 })
-export class DeleteEventComponent {
-  constructor(private router: Router) {}
+export class DeleteEventComponent implements OnInit {
+  event: AppEvent | undefined;
+  submitting: boolean = false;
+  errorMessage: string | undefined = undefined;
+  constructor(
+    private router: Router,
+    private location: Location,
+    private adminEventService: AdminEventService
+  ) {}
+  ngOnInit(): void {
+    this.eventInit();
+  }
+
+  eventInit(): void {
+    const state = this.location.getState() as RouteState;
+    this.event = state.event;
+
+    if (!state.event) {
+      this.router.navigate(['admin', 'events']);
+    }
+  }
+
+  submit(): void {
+    if (!this.event?.id) return;
+    this.submitting = true;
+
+    this.adminEventService
+      .deleteEvent(this.event.id)
+      .pipe(take(1))
+      .subscribe({
+        next: (response) => {
+          console.warn('update response', response);
+          this.router.navigate(['admin', 'events']);
+        },
+        error: (error) => {
+          this.submitting = false;
+          this.errorMessage = error.errorMessage;
+        },
+        complete: () => {
+          this.submitting = false;
+        },
+      });
+  }
+
+  dismissError($event: boolean) {
+    if ($event) {
+      this.errorMessage = undefined;
+    }
+  }
 
   dismiss(): void {
     this.router.navigate(['admin', 'events']);
