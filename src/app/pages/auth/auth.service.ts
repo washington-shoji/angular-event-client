f3066ac9a302f903e174505861fbde857dd5eaa3
washington-shoji <shoji.washington@gmail.com>
2024-06-29 08:37:06 +1000

    - updated auth service logic

--- src/app/pages/auth/auth.service.ts
+++ src/app/pages/auth/auth.service.ts
@@ -1,6 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export type UserAuth = {
+  username: string;
+  email: string;
+  password: string;
+};
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  URL: string = 'http://localhost:3000/api/v1';
+
+  login(payload: UserAuth): Observable<void> {
+    return this.http.post<void>(`${this.URL}/login`, payload);
+  }
 }
