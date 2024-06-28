83ab7a5f61c4e9b82b25f4f8cc323da2a1f1c050
washington-shoji <shoji.washington@gmail.com>
2024-06-28 18:26:23 +1000

    - updated admin-events component logic

---
+++ src/app/pages/admin/admin-events/admin-events.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { TableComponent } from '../../../components/table/table/table.component';
+import { AdminEventService } from '../services/admin-event.service';
+import { take } from 'rxjs';
+import { AppEvent } from '../../public-event-page/event.type';
+
+@Component({
+  selector: 'app-admin-events',
+  standalone: true,
+  imports: [TableComponent],
+  providers: [AdminEventService],
+  templateUrl: './admin-events.component.html',
+  styleUrl: './admin-events.component.scss',
+})
+export class AdminEventsComponent implements OnInit {
+  events: AppEvent[] = [];
+  loading: boolean = false;
+  errorMessage: string | undefined = undefined;
+  constructor(private adminEventService: AdminEventService) {}
+
+  ngOnInit(): void {
+    this.getEventsInit();
+  }
+
+  getEventsInit(): void {
+    this.loading = true;
+    this.adminEventService
+      .getEvents()
+      .pipe(take(1))
+      .subscribe({
+        next: (data) => {
+          this.events = data;
+        },
+        error: (error) => {
+          this.loading = false;
+          this.errorMessage = error.errorMessage;
+        },
+        complete: () => {
+          this.loading = false;
+        },
+      });
+  }
+}
