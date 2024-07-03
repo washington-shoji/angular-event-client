0080b98d65a40726eec6552cd63594126fb1e0ea
washington-shoji <shoji.washington@gmail.com>
2024-07-04 08:53:16 +1000

    - updated admin layout component

--- src/app/components/admin-layout-screen/admin-layout-screen.component.ts
+++ src/app/components/admin-layout-screen/admin-layout-screen.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { AdminNavbarComponent } from '../admin-navbar/admin-navbar.component';
 import { AdminSidebarComponent } from '../admin-sidebar/admin-sidebar.component';
 import { RouterModule } from '@angular/router';
+import { SessionStorageService } from '../../services/session-storage/session-storage.service';
 
 @Component({
   selector: 'app-admin-layout-screen',
   standalone: true,
   imports: [RouterModule, AdminNavbarComponent, AdminSidebarComponent],
+  providers: [SessionStorageService],
   templateUrl: './admin-layout-screen.component.html',
   styleUrl: './admin-layout-screen.component.scss',
 })
