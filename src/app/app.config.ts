1b2442003fd9dbf6750ede415b9dbd3e9ab885f5
washington-shoji <shoji.washington@gmail.com>
2024-07-04 08:51:54 +1000

    - updated app config with interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { SessionStorageService } from './services/session-storage/session-storage.service';
+import { apiAdminInterceptor } from './interceptors/api/api-admin/api-admin.interceptor';
+import { AuthService } from './pages/auth/auth.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([apiAdminInterceptor])),
+    SessionStorageService,
+    AuthService,
+  ],
 };
