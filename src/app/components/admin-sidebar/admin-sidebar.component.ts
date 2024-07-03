7df3fb4489f9cccb4a8d02ef67ac182b634da7f8
washington-shoji <shoji.washington@gmail.com>
2024-07-04 08:53:37 +1000

    - updated admin side bar component

--- src/app/components/admin-sidebar/admin-sidebar.component.ts
+++ src/app/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { AuthService } from '../../pages/auth/auth.service';
 
 @Component({
   selector: 'app-admin-sidebar',
   standalone: true,
   imports: [CommonModule],
+  providers: [AuthService],
   templateUrl: './admin-sidebar.component.html',
   styleUrl: './admin-sidebar.component.scss',
 })
 export class AdminSidebarComponent {
   sidebarMenuOpen = input.required<boolean>();
+
+  constructor(private authService: AuthService) {}
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
