88fa874d0fcc1007961f547e00c6aab8f092aad7
washington-shoji <shoji.washington@gmail.com>
2024-07-14 10:47:44 +1000

    - added event address form component

---
+++ src/app/components/form/address-form/address-form.component.ts
@@ -0,0 +1,76 @@
+import { Component, input, OnInit, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { EventAddress } from '../../../types/event-address';
+
+@Component({
+  selector: 'app-address-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './address-form.component.html',
+  styleUrl: './address-form.component.scss',
+})
+export class AddressFormComponent implements OnInit {
+  eventAddress = input.required<EventAddress>();
+  submitting = input.required<boolean>();
+  addressFrmData = output<FormGroup>();
+
+  addressFrm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.addressFrm = new FormGroup({});
+  }
+  ngOnInit(): void {
+    this.addressFormInit();
+  }
+
+  addressFormInit(): void {
+    this.addressFrm = this.formBuilder.group({
+      street: [this.eventAddress()?.street ?? null, Validators.required],
+      city_suburb: [
+        this.eventAddress()?.city_suburb ?? null,
+        Validators.required,
+      ],
+      state: [this.eventAddress()?.state ?? null, Validators.required],
+      country: [this.eventAddress()?.country ?? null, Validators.required],
+      postal_code: [
+        this.eventAddress()?.postal_code ?? null,
+        Validators.required,
+      ],
+    });
+  }
+
+  submit(): void {
+    if (!this.addressForm.valid) return;
+    this.addressFrmData.emit(this.addressForm);
+  }
+
+  get addressForm(): FormGroup {
+    return this.addressFrm;
+  }
+
+  get streetControl(): FormControl {
+    return this.addressForm.get('street') as FormControl;
+  }
+
+  get citySuburbControl(): FormControl {
+    return this.addressForm.get('city_suburb') as FormControl;
+  }
+
+  get stateControl(): FormControl {
+    return this.addressForm.get('state') as FormControl;
+  }
+
+  get countryControl(): FormControl {
+    return this.addressForm.get('country') as FormControl;
+  }
+
+  get postalCodeControl(): FormControl {
+    return this.addressForm.get('postal_code') as FormControl;
+  }
+}
