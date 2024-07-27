45da0d166e731ea5bf2d6f2cfc4a9456837ab53a
washington-shoji <shoji.washington@gmail.com>
2024-07-28 08:27:05 +1000

    - updated event and public event service

--- src/app/pages/admin/services/admin-event.service.ts
+++ src/app/pages/admin/services/admin-event.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AppEvent } from '../../public-event-page/event.type';
+import { AppEvent } from '../../../types/event';
 
 @Injectable()
 export class AdminEventService {
--- src/app/pages/public-event-page/public-event.service.ts
+++ src/app/pages/public-event-page/public-event.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AppEvent } from './event.type';
+import { AppEvent } from '../../types/event';
 
 @Injectable()
 export class PublicEventService {
