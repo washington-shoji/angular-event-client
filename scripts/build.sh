7777b5b018b8cc8526d437401276be664985df5e
washington-shoji <shoji.washington@gmail.com>
2024-09-15 08:03:36 +1000

    - added build bash script

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Replace placeholders in environment.prod.ts.template
+envsubst < src/environments/environment.prod.ts.template > src/environments/environment.prod.ts
+
+# Build the Angular app
+ng build --prod
