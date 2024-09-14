3d0791bd6c97d794ba63a01577afabdd1ff71ba0
washington-shoji <shoji.washington@gmail.com>
2024-09-15 09:29:19 +1000

    - updated ci workflow to build app statically in vercel

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace placeholders in environment.prod.ts.template
-envsubst < src/environments/environment.prod.ts.template > src/environments/environment.prod.ts
-
-# Build the Angular app
-ng build --configuration production
