749be7b0ac6360437f433dfa8161be40bd8cc088
washington-shoji <shoji.washington@gmail.com>
2025-02-08 11:00:22 +1100

    - updated data types

--- src/app/types/event-registered.ts
+++ src/app/types/event-registered.ts
@@ -1,9 +1,11 @@
 import { AppEvent } from './event';
 import { EventAddress } from './event-address';
 import { EventAttendee } from './event-attendee';
+import { EventImage } from './event-image';
 
 export interface RegisteredEvent {
   attendee: EventAttendee;
   event: AppEvent;
   address: EventAddress;
+  image?: EventImage;
 }
